cbd199809b6a36c94eb990920cd7857f4478e1a0
cdilday <seesevn@cox.net>
2014-06-02 23:38:53 -0700

    Fixed the inflate problem
    Turns out the meta data needs to be put inside the <application> tags in
    the xml. But it should work for both our keys now.

--- EventPlanner/src/com/example/eventplanner/MakeEventActivity.java
+++ EventPlanner/src/com/example/eventplanner/MakeEventActivity.java
@@ -2,6 +2,7 @@ package com.example.eventplanner;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,7 +14,7 @@ import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
-public class MakeEventActivity extends ActionBarActivity {
+public class MakeEventActivity extends FragmentActivity {
 
 	private GoogleMap googleMap;
 	
